3ced44bea1f50a1b882ad620ebddf71af1eb15a1
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2017-09-08 12:37:00 +0200

    added unix epoch converter, and utm_to_gps for NVDB coordinates

--- util.py
+++ util.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import calendar
+import utm
+
+
+def datetime_to_unix_epoch(timestamp):
+    return calendar.timegm(timestamp.timetuple())
+
 
 def parse_timestamp(timestamp):
     return datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+
+
+def utm_to_gps(utm_coordinate, zone=33, zone_letter='N'):
+    """
+    Converts UTM coordinate to GPS lat, lon.
+
+    Defaults to zone number 33 (Norway) and letter 'N'.
+    """
+    print("utm_coordinate: {}".format(utm_coordinate))
+    return utm.to_latlon(utm_coordinate[0], utm_coordinate[1], zone, zone_letter)
