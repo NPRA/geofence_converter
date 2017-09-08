0cea512512d2a6fb95d767be89ed0e34c094da1c
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2017-09-08 16:01:05 +0200

    fixed duplicates and remove pkg_resources bug

--- client.py
+++ client.py
@@ -4,6 +4,8 @@
 from __future__ import print_function, unicode_literals
 import argparse
 import logging
+import yaml
+import os
 import sys
 import geofence
 import storage
@@ -19,35 +21,70 @@ log.addHandler(ch)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--broker_url", help="AMQP schemed url to the broker/AMQP server.", required=True)
+    parser.add_argument("-conf", "--config", help="Config file that specifies all input parameters", default=None)
+    parser.add_argument("-b", "--broker_url", help="AMQP schemed url to the broker/AMQP server.", default=None)
     parser.add_argument("-v", "--verbose", help="Enable verbose logging / printing", default=False)
-    parser.add_argument("-s", "--sender", help="Name of the sender queue", required=True)
-    parser.add_argument("-r", "--receiver", help="Name of the receiver queue", required=True)
+    parser.add_argument("-s", "--sender", help="Name of the sender queue", default=None)
+    parser.add_argument("-r", "--receiver", help="Name of the receiver queue", default=None)
     parser.add_argument("-k", "--ssl-keyfile", help="SSL key file")
     parser.add_argument("-c", "--ssl-certfile", help="SSL cert file")
     parser.add_argument("-u", "--username", help="Username", default=None)
     parser.add_argument("-p", "--password", help="Password", default=None)
 
-    args = parser.parse_args()
 
-    if args.verbose:
-        pass
+    args = parser.parse_args()
+    cfg = {}
+
+    if args.config:
+        print("Using config file")
+        if not os.path.exists(args.config):
+            print("Config file was not found..")
+            sys.exit(1)
+        with open(args.config, 'r') as f:
+            cfg = yaml.load(f)
+
+        required_parms = []
+        cfg_get = lambda x: cfg.get(x, False)
+        map(required_parms.append, [cfg_get("broker_url"), cfg_get("sender"), cfg_get("receiver")])
+        if not all(required_parms):
+            log.error("Missing required parameters from config file!")
+            sys.exit(1)
+
+    else:
+
+        required_parms = []
+        map(required_parms.append, [args.broker_url, args.sender, args.receiver])
+        print("required_parms: {}".format(required_parms))
+
+        if not all(required_parms):
+            print("Missing required parameters!")
+        sys.exit(1)
+
+        import pdb; pdb.set_trace()
+
+    if cfg.get("verbose", False):
+        log.setLevel(logging.DEBUG)
 
     options = {"ssl_skip_hostname_check": True}
-    if args.ssl_keyfile:
-        options.update({"ssl_keyfile": args.ssl_keyfile})
-    if args.ssl_certfile:
-        options.update({"ssl_certfile": args.ssl_certfile})
+    if cfg.get("ssl_keyfile", False):
+        options.update({"ssl_keyfile": cfg.get("ssl_keyfile")})
+
+    if cfg.get("ssl_certfile", False):
+        options.update({"ssl_certfile": cfg.get("ssl_certfile")})
     print("options: {}".format(options))
 
-    if args.broker_url.startswith("amqps"):
+    if cfg.get("broker_url", "").startswith("amqps"):
         # Encrypted amqps session
-        if not all([args.ssl_keyfile, args.ssl_certfile]):
+        if not all([cfg.get("ssl_keyfile"), cfg.get("ssl_certfile")]):
             log.error("Broker URL uses TLS/SSL. Therefore you need to specify SSL cert and key.")
             sys.exit(1)
 
-    ic = NordicWayIC(args.broker_url, args.sender, args.receiver,
-                     args.username, args.password, options)
+    ic = NordicWayIC(cfg.get("broker_url"),
+                     cfg.get("sender"),
+                     cfg.get("receiver"),
+                     cfg.get("username"),
+                     cfg.get("password"),
+                     options)
     log.debug(ic)
     ic.connect()
 
@@ -60,6 +97,8 @@ if __name__ == '__main__':
         fences = geofence.fetch_objects()
         if not fences or fences.get("returnert", 0) == 0:
             log.debug("No geofence objects..")
+            time.sleep(5)
+            continue
 
         # TODO: Check if returned JSON has paging. If so, fetch the rest of
         #       the geofence objects
