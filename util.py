21fdc8fd1522a7b502bfa50cd4144ebe74daa0fb
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2017-09-11 15:36:48 +0200

    remove old print stmt

--- util.py
+++ util.py
@@ -19,5 +19,4 @@ def utm_to_gps(utm_coordinate, zone=33, zone_letter='N'):
 
     Defaults to zone number 33 (Norway) and letter 'N'.
     """
-    print("utm_coordinate: {}".format(utm_coordinate))
     return utm.to_latlon(utm_coordinate[0], utm_coordinate[1], zone, zone_letter)
