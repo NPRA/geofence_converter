27d2f2aff73ff389ee3452c0e4b066dded3ca7da
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2017-09-19 13:20:36 +0200

    fixed testcase

--- test_datex2.py
+++ test_datex2.py
@@ -22,13 +22,21 @@ class TestDatex2(TestCase):
     def test_add_body(self):
         d = Datex2()
 
-        name = "TestName"
-        nvdb_id = "123456"
+        name = "Oslo Demo"
+        nvdb_id = "826277828"
         unix_epoch = calendar.timegm(time.gmtime())
-        polygon = [(69.273653333, 19.944328333), (69.27362, 19.944135)]
+        # Polygon, Oslo, Grønland
+        polygon = [(262906.3971474045, 6649248.441221254),
+                   (263059.8558061711, 6649187.586932589),
+                   (263022.81406153727, 6649124.086805921),
+                   (263816.5657456318, 6648542.002307889),
+                   (263943.56601331057, 6648669.00256108),
+                   (263803.3365402619, 6650023.6719330065),
+                   (262906.3971474045, 6649248.441221254)]
 
         d.body(name, nvdb_id, unix_epoch, polygon)
         print(d)
+        
 
-        self.assertTrue(d.doc.findall("publicationTime"))
+        self.assertTrue(d.doc.findall("payloadPublication"))
         self.assertTrue(d.doc.findall("location"))
